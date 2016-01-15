cda3e60c7dc21112872de3fc80ed0e080036c3af
Bomby K <bombyk@gmail.com>
2016-01-14 15:35:54 -0900

    useraccount master and pages, new css files
    Added files:
    Account/Rooms
    Account/Prioritization
    Account/Details
    Account/Contact
    Content/sidebar.css
    
    Updates to:
    Site.Master
    UserAccount.Master
    Content/site.css

---
+++ Waitlist_Transparency_System/Account/Rooms.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Waitlist_Transparency_System.Account
+{
+    public partial class Rooms : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void UpdateAgeRange(object sender, EventArgs e)
+        {
+            lblAgeRangeUpdate.Visible = true;
+            lblAgeRangeUpdate.CssClass = "label label-success";
+            lblAgeRangeUpdate.Text = "Age range updated.";
+        }
+
+        protected void UpdateWaitlist(object sender, EventArgs e)
+        {
+            lblWaitlistUpdate.Visible = true;
+            lblWaitlistUpdate.CssClass = "label label-danger";
+            lblWaitlistUpdate.Text = "Uh oh! Waitlist failed to update.";
+        }
+
+    }
+}
